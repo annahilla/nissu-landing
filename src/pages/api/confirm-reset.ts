560819ead804eb4deb13b229a5501f01bdce0cde
Anna Hilla <137043715+annahilla@users.noreply.github.com>
2025-06-03 12:12:20 +0200

    Add layout, button and linkbutton components

--- src/pages/api/confirm-reset.ts
+++ src/pages/api/confirm-reset.ts
@@ -1,5 +1,5 @@
-import type { APIRoute } from "astro";
-import { Client, Account } from "node-appwrite";
+import type { APIRoute } from 'astro';
+import { Client, Account } from 'node-appwrite';
 
 const client = new Client()
   .setEndpoint(import.meta.env.APPWRITE_ENDPOINT)
@@ -10,27 +10,27 @@ const account = new Account(client);
 
 export const POST: APIRoute = async ({ request, redirect }) => {
   const data = await request.formData();
-  const token = data.get("token")?.toString();
-  const secret = data.get("secret")?.toString();
-  const expiresAt = data.get("expiresAt")?.toString();
-  const password = data.get("password")?.toString();
-  const confirmPassword = data.get("confirmPassword")?.toString();
+  const token = data.get('token')?.toString();
+  const secret = data.get('secret')?.toString();
+  const expiresAt = data.get('expiresAt')?.toString();
+  const password = data.get('password')?.toString();
+  const confirmPassword = data.get('confirmPassword')?.toString();
 
   const expiresAtNumber = Number(expiresAt);
 
   if (!token || !secret || !password || password !== confirmPassword) {
-    return new Response("Invalid request", { status: 400 });
+    return new Response('Invalid request', { status: 400 });
   }
 
   if (expiresAtNumber < Date.now()) {
-    return new Response("Token invalid or expired", { status: 400 });
+    return new Response('Token invalid or expired', { status: 400 });
   }
 
   try {
     await account.updateRecovery(token, secret, password);
-    return redirect("/reset-password-success");
+    return redirect('/reset-password/success');
   } catch (err) {
-    console.error("Reset error:", err);
-    return new Response("Internal error", { status: 500 });
+    console.error('Reset error:', err);
+    return new Response('Internal error', { status: 500 });
   }
 };
