49d2a6056e7511ff5e6f76c02fc5600e5fa24c23
Anna Hilla <137043715+annahilla@users.noreply.github.com>
2025-06-03 10:56:42 +0200

    Update reset password logic

--- src/pages/api/confirm-reset.ts
+++ src/pages/api/confirm-reset.ts
@@ -1,46 +1,33 @@
 import type { APIRoute } from "astro";
-import { Client, Users, Databases, Query } from "node-appwrite";
+import { Client, Account } from "node-appwrite";
 
 const client = new Client()
   .setEndpoint(import.meta.env.APPWRITE_ENDPOINT)
   .setProject(import.meta.env.APPWRITE_PROJECT_ID)
   .setKey(import.meta.env.APPWRITE_API_KEY);
 
-const users = new Users(client);
-const databases = new Databases(client);
+const account = new Account(client);
 
 export const POST: APIRoute = async ({ request, redirect }) => {
   const data = await request.formData();
   const token = data.get("token")?.toString();
+  const secret = data.get("secret")?.toString();
+  const expiresAt = data.get("expiresAt")?.toString();
   const password = data.get("password")?.toString();
   const confirmPassword = data.get("confirmPassword")?.toString();
 
-  if (!token || !password || password !== confirmPassword) {
+  const expiresAtNumber = Number(expiresAt);
+
+  if (!token || !secret || !password || password !== confirmPassword) {
     return new Response("Invalid request", { status: 400 });
   }
 
+  if (expiresAtNumber < Date.now()) {
+    return new Response("Token invalid or expired", { status: 400 });
+  }
+
   try {
-    const res = await databases.listDocuments(
-      import.meta.env.APPWRITE_DB,
-      import.meta.env.APPWRITE_COL_DELETE_TOKENS_ID,
-      [Query.equal("token", token)]
-    );
-
-    const resetDoc = res.documents[0];
-    if (!resetDoc || resetDoc.expiresAt < Date.now()) {
-      return new Response("Token invalid or expired", { status: 400 });
-    }
-
-    const userId = resetDoc.userId;
-
-    await users.updatePassword(userId, password);
-
-    await databases.deleteDocument(
-      import.meta.env.APPWRITE_DB,
-      import.meta.env.APPWRITE_COL_DELETE_TOKENS_ID,
-      resetDoc.$id
-    );
-
+    await account.updateRecovery(token, secret, password);
     return redirect("/reset-password-success");
   } catch (err) {
     console.error("Reset error:", err);
